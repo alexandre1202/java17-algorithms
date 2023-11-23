2246b4c3a2abc125b940eb28cd407ffc0a1f1f86
Alexandre A Barbosa <alexandre1202@gmail.com>
2023-11-23 16:05:16 -0300

    Finding the max average of the subarray fixed

--- src/main/java/br/com/aab/leetcode/dsa/arraysandstrings/maximumaveragesubarray/FindSubArrayByMaxAverageK.java
+++ src/main/java/br/com/aab/leetcode/dsa/arraysandstrings/maximumaveragesubarray/FindSubArrayByMaxAverageK.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class FindSubArrayByMaxAverageK {
     private double findMaxAverageByK(int[] nums, int k) {
+        if (nums.length == 1) return nums[0];
         double sum = 0;
         double maxAverage = 0;
         for (int i = 0; i < nums.length; i++) {
@@ -11,7 +12,7 @@ public class FindSubArrayByMaxAverageK {
             if (i >= k-1) {
                 if (i > k-1) sum -= nums[i-k];
                 double average = sum / k;
-                maxAverage = Math.max(average, maxAverage);
+                maxAverage = (i+1) == k ? average : Math.max(average, maxAverage);
             }
         }
         return maxAverage;
@@ -30,5 +31,19 @@ public class FindSubArrayByMaxAverageK {
         result = finder.findMaxAverageByK(nums, k);
         System.out.printf("The max average for the array %s with window [%d] is [%f]%n",
                 Arrays.toString(nums), k, result);
+
+        nums = new int[]{-1};
+        k = 1;
+        result = finder.findMaxAverageByK(nums, k);
+        System.out.printf("The max average for the array %s with window [%d] is [%f]%n",
+                Arrays.toString(nums), k, result);
+
+        nums = new int[]{8860,-853,6534,4477,-4589,8646,-6155,-5577,-1656,-5779,-2619,-8604,-1358,-8009,4983,7063,3104,-1560,4080,2763,5616,-2375,2848,1394,-7173,-5225,-8244,-809,8025,-4072,-4391,-9579,1407,6700,2421,-6685,5481,-1732,-8892,-6645,3077,3287,-4149,8701,-4393,-9070,-1777,2237,-3253,-506,-4931,-7366,-8132,5406,-6300,-275,-1908,67,3569,1433,-7262,-437,8303,4498,-379,3054,-6285,4203,6908,4433,3077,2288,9733,-8067,3007,9725,9669,1362,-2561,-4225,5442,-9006,-429,160,-9234,-4444,3586,-5711,-9506,-79,-4418,-4348,-5891};
+        k = 93;
+        //result = 3768.28.571
+        result = finder.findMaxAverageByK(nums, k);
+        System.out.printf("The max average for the array %s with window [%d] is [%f]%n",
+                Arrays.toString(nums), k, result);
+
     }
 }
