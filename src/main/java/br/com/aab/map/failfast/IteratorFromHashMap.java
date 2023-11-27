f884822961c50ea94c1e143a27fbc36bf7707d64
Alexandre A Barbosa <alexandre1202@gmail.com>
2023-11-27 15:58:14 -0300

    Interesting fail-fast iterator

--- src/main/java/br/com/aab/map/failfast/IteratorFromHashMap.java
+++ src/main/java/br/com/aab/map/failfast/IteratorFromHashMap.java
@@ -1,4 +1,4 @@
-package br.com.aab.map.iteratorhashmap;
+package br.com.aab.map.failfast;
 
 import java.util.HashMap;
 import java.util.Iterator;
@@ -9,6 +9,8 @@ public class IteratorFromHashMap {
      * In Java, the iterator returned by HashMap is fail-fast. This means that if the underlying collection
      * is modified after the iterator is created, the iterator will throw a ConcurrentModificationException
      * to indicate that the collection has been concurrently modified.
+     * But this behavior occurs only when we add or remove data, I mean, changing the structure.
+     * The exception does not occur when we change the value of existent data.
      */
 
     private static final Map<String, String> students = new HashMap<>(){{
@@ -21,12 +23,18 @@ public class IteratorFromHashMap {
 
     private static void triggerConcurrentModificationException() {
         Iterator<Map.Entry<String, String>> iteratorStudents = students.entrySet().iterator();
-        while (iteratorStudents.hasNext()) {
-            Map.Entry<String, String> studentsEntry = iteratorStudents.next();
-            if ("John".equals(studentsEntry.getKey())) {
-                System.out.printf("Vai lançar a exception!!! %n");
-                students.put("John", "MT");
+        try {
+            while (iteratorStudents.hasNext()) {
+                Map.Entry<String, String> studentsEntry = iteratorStudents.next();
+                System.out.println(studentsEntry.getKey() + ": " + studentsEntry.getValue());
+
+                if ("John".equals(studentsEntry.getKey())) {
+                    System.out.printf("Vai lançar a exception!!! %n");
+                    students.put("Stanley", "MT");
+                }
             }
+        } catch (Exception e) {
+            System.out.printf("Exception bizzara due to fail-fast iterator %s%n", e);
         }
     }
 
