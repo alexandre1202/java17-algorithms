258619ff2000aa316d9fdb55f0753f58c8b43f83
Alexandre A Barbosa <alexandre1202@gmail.com>
2024-01-02 23:56:02 -0300

    feat: improvments for AnagramFinder

--- src/main/java/br/com/aab/algorithms/AnagramFinder.java
+++ src/main/java/br/com/aab/algorithms/AnagramFinder.java
@@ -7,12 +7,14 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- * Context for Candidate: Given a list of words, return all groups of words that are anagrams of each other.
- *  An anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using
- *  all the original letters exactly once.
- *  Input:
+ * Context for Candidate: Given a list of words, return all groups
+ *    of words that are anagrams of each other.
+ * An anagram is a word or phrase formed by rearranging the letters
+ *    of a different word or phrase, typically using
+ *    all the original letters exactly once.
+ * Input:
  *      "listen", "integral", "enlist", "inlets","inlets", "silent", "earth", "apple", "heart", "earth", "cinema", "iceman", "triangle"
- *  Expected output:
+ * Expected output:
  *      [earth, heart]
  *      [inlets, silent, enlist, listen]
  *      [integral, triangle]
@@ -22,9 +24,10 @@ import java.util.stream.Collectors;
 public class AnagramFinder {
     public List<List<String>> findAnagrams(String[] words) {
         return Arrays.stream(words)
-                .collect(Collectors.groupingBy(this::sortedWord, HashMap::new, Collectors.toSet()))
-                .values().stream().map(ArrayList::new)
-                .collect(Collectors.toList());
+                .collect(Collectors.groupingBy(
+                        this::sortedWord, HashMap::new, Collectors.toSet()
+                ))
+                .values().stream().map(ArrayList::new).collect(Collectors.toList());
     }
     private String sortedWord(String word) {
         char[] charWord = word.toCharArray();
