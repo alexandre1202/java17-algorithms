2d54ddd3f39667593db77acf49416c74370b61db
Alexandre A Barbosa <alexandre1202@gmail.com>
2024-02-29 17:04:25 -0300

    feat: simple and useful

--- src/main/java/br/com/aab/javacodingproblems/s004/CheckingOnlyDigits.java
+++ src/main/java/br/com/aab/javacodingproblems/s004/CheckingOnlyDigits.java
@@ -2,6 +2,18 @@ package br.com.aab.javacodingproblems.s004;
 
 public class CheckingOnlyDigits {
     public boolean onlyDigits(String input) {
-        return input.chars().anyMatch(d -> Character.isDigit(d));
+        return input.chars().anyMatch(Character::isDigit);
+    }
+
+
+    public static void main(String[] args) {
+        // Defining the codepoint values
+        // 57-> 9     95 -> _     9 -> TAB
+        int a = 57, b = 95, c = 9;
+
+        // Applying the isDigit() function
+        System.out.println(a + ": a : " + Character.isDigit(a));
+        System.out.println(b + ": b : " + Character.isDigit(b));
+        System.out.println(c + ": c : " + Character.isDigit(c));
     }
 }
