275634f12987a40d9c0c42328b5e60a5088e7f97
Alexandre A Barbosa <alexandre1202@gmail.com>
2024-03-07 21:41:17 -0300

    feat: updating the style

--- src/main/java/br/com/aab/algorithms/FindingTheMissingNumber.java
+++ src/main/java/br/com/aab/algorithms/FindingTheMissingNumber.java
@@ -5,7 +5,7 @@ import java.util.IntSummaryStatistics;
 import java.util.Objects;
 
 public class FindingTheMissingNumber {
-    static long missingNumberV1(int array[]) {
+    public long missingNumberV1(int[] array) {
         if (Objects.isNull(array) || array.length < 2) return array[0];
         IntSummaryStatistics iss = Arrays.stream(array).summaryStatistics();
         long lower = iss.getMin(), high = iss.getMax(), sum = iss.getSum();
