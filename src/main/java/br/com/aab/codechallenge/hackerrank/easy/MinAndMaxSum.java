587fb949295b21a352f8ed03ada9610b93df2cda
Alexandre A Barbosa <alexandre1202@gmail.com>
2024-03-07 21:51:14 -0300

    feat: rename and adjustment on main test

--- src/main/java/br/com/aab/codechallenge/hackerrank/easy/MinAndMaxSum.java
+++ src/main/java/br/com/aab/codechallenge/hackerrank/easy/MinAndMaxSum.java
@@ -1,9 +1,13 @@
 package br.com.aab.codechallenge.hackerrank.easy;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class MiniMaxSum {
+/**
+ * https://www.hackerrank.com/challenges/mini-max-sum/problem?isFullScreen=true
+ */
+public class MinAndMaxSum {
     public void miniMaxSumUsingSort(List<Integer> arr) {
         /**
          * Using sort
@@ -12,7 +16,7 @@ public class MiniMaxSum {
         long sum = arr.stream().mapToInt(i -> i).sum();
         long left = sum - arr.get(arr.size() - 1);
         long right = sum - arr.get(0);
-        System.out.printf("%d %d", left, right);
+        System.out.printf("%d %d%n", left, right);
     }
 
     public void miniMaxSumUsingWithoutSort(List<Integer> arr) {
@@ -35,12 +39,16 @@ public class MiniMaxSum {
     }
 
     public static void main(String[] args) {
-        MiniMaxSum miniMaxSum = new MiniMaxSum();
+        MinAndMaxSum minAndMaxSum = new MinAndMaxSum();
 
-        var input1 = List.of(1, 3, 5, 7, 9); //Result 16 24
-        miniMaxSum.miniMaxSumUsingWithoutSort(input1);
+        var immutableInput1 = List.of(1, 3, 5, 7, 9); //Result 16 24
+        var mutableInput1 = new ArrayList<>(immutableInput1);
+        minAndMaxSum.miniMaxSumUsingSort(mutableInput1);
+        minAndMaxSum.miniMaxSumUsingWithoutSort(immutableInput1);
 
-        var input2 = List.of(1, 2, 3, 4, 5); //Result 10 14
-        miniMaxSum.miniMaxSumUsingWithoutSort(input2);
+        var immutableList2 = List.of(1, 2, 3, 4, 5); //Result 10 14
+        var mutableList2 = new ArrayList<>(immutableList2);
+        minAndMaxSum.miniMaxSumUsingSort(mutableList2);
+        minAndMaxSum.miniMaxSumUsingWithoutSort(immutableList2);
     }
 }
