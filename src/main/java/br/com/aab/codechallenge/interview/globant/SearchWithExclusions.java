ac7b870383defb07ba72214e5c774616e101733c
Alexandre A Barbosa <alexandre1202@gmail.com>
2024-03-20 21:47:29 -0300

    feat: code challenges with search and exclusions

--- src/main/java/br/com/aab/codechallenge/interview/globant/SearchWithExclusions.java
+++ src/main/java/br/com/aab/codechallenge/interview/globant/SearchWithExclusions.java
@@ -8,6 +8,15 @@ import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+/**
+ * Implement a search method that excludes a given data from a fixed list of
+ * alphabetic letters [a, b, c, ... z]
+ * i.e.
+ * if the query is "Mickey" you are expected to exclude [a, b, c, d] from the result
+ * if the query is "Minnie" you are expected to exclude [d, e, b] from the result
+ * if the query is "Disney" you are expected to exclude [d, e, y] from the result
+ * Otherwise the result is a full list of the
+ */
 public class SearchWithExclusions {
 
     private final String alphabet = "abcdefghijklmnopqrstuvxz";
